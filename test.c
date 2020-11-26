20861e68894154f4a1e05c1c2e4c06b7a9998ca7
Abderrazzaq Khastaf <akhastaf@student.1337.ma>
2020-11-26 14:33:05 +0100

    adding file akhastaf

--- test.c
+++ test.c
@@ -11,27 +11,49 @@ typedef struct  s_cmd
     char    **arg;
     char    *opr;
     struct s_cmd   *next;
+    struct s_cmd   *prev;
+    
 }           t_cmd;
 int g_fd[2];
 void    my_execute(t_cmd cmdlist);
-void    set_pipe();
+void    set_pipe_w();
+void    set_pipe_r();
 
-void    set_pipe()
+void    set_pipe_w()
+{
+    pipe(g_fd);
+    dup2(g_fd[1], 1);
+    close(g_fd[0]);
+    close(g_fd[1]);
+}
+void    set_pipe_r()
 {
     pipe(g_fd);
     dup2(g_fd[0], 0);
+    close(g_fd[0]);
+    close(g_fd[1]);
 }
 void    my_execute(t_cmd cmdlist)
 {
     pid_t pid;
 
-    if (cmdlist.opr)
-        if (!strcmp(cmdlist.opr, "|"))
-            set_pipe();
+    
     pid = fork();
     waitpid(pid, NULL, 0);
     if (pid == 0)
     {
+        printf("%s\n", cmdlist.path);
+        if (cmdlist.opr)
+            if (!strcmp(cmdlist.opr, "|"))
+                set_pipe_w();
+        if (cmdlist.prev)
+        {
+            if (cmdlist.prev->opr)
+            {
+                if (!strcmp(cmdlist.prev->opr, "|"))
+                    set_pipe_r();
+            }
+        }
         execve(cmdlist.path, cmdlist.arg, NULL);
     }
 }
@@ -41,8 +63,8 @@ int     main()
     t_cmd *cmdlist;
     pid_t pid;
     t_cmd *cmd;
-    char *a[] = { "/sbin/ping", "-c", "2", "google.com", NULL };
-    char *b[] = { "/bin/ls", "-all", NULL };
+    char *b[] = { "/usr/bin/grep", "statistics", NULL };
+    char *a[] = { "/sbin/ping", "-c", "4", "google.com", NULL };
     
     cmdlist = malloc(sizeof(t_cmd));
     cmd = malloc(sizeof(t_cmd));
@@ -51,18 +73,18 @@ int     main()
     cmdlist->arg = a;
     cmdlist->opr = "|";
     cmdlist->next = cmd;
+    cmdlist->prev = NULL;
 
-    cmd->path = "/bin/ls";
+    cmd->path = "/usr/bin/grep";
     cmd->arg = b;
     cmd->opr = NULL;
     cmd->next = NULL;
-
-
+    cmd->prev = cmdlist;
 
     while (cmdlist)
     {
-        printf("%s\n", cmdlist->path);
-        my_execute(*cmdlist);   
+        my_execute(*cmdlist);
+        //printf("%s\n", cmdlist->path);  
         cmdlist = cmdlist->next;
     }
 }
